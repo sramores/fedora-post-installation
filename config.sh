8fbd6351b3463f121000a3c1c8bcd5732c53aa29
Sergio Rodriguez <sramores@gmail.com>
2022-12-07 12:40:41 +0100

    Update Config

--- config.sh
+++ config.sh
@@ -1,10 +1,13 @@
 sudo dnf -y --refresh upgrade
 
-sudo dnf install git curl zsh zsh-syntax-highlighting code neofetch -y
+sudo dnf -y install git curl redshift neofetch rofi ranger kitty gh
+
+
+#zsh zsh-syntax-highlighting -y
 
 # Make zsh the default shell
-chsh -s /usr/bin/zsh
+#chsh -s /usr/bin/zsh
 
 # Get oh-my-zsh and install it
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
