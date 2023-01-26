a8555d53fe168c657e8570c2a8c9f8bc15fa2d99
Sergio Rodriguez <sramores@gmail.com>
2023-01-26 19:35:42 +0100

    vscode

--- config.sh
+++ config.sh
@@ -1,6 +1,19 @@
 sudo dnf -y --refresh upgrade
 
-sudo dnf -y install git curl redshift neofetch rofi ranger kitty gh btop
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo dnf check-update
+
+sudo dnf -y install git curl redshift neofetch rofi ranger kitty gh btop mpv code
 
 
 #zsh zsh-syntax-highlighting -y
